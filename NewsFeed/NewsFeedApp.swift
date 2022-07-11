e4d1c342fb20baa171b1333e7faf9748e21e027e
Mykyta Oliinyk <oleynikita1@gmail.com>
2022-07-11 18:13:19 +0300

    created empty project

---
+++ NewsFeed/NewsFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsFeedApp.swift
+//  NewsFeed
+//
+//  Created by Mykyta Oliinyk on 11.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct NewsFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
